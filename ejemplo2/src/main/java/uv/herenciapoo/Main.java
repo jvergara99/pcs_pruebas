965104f1125dcb9e064c1509eaec15dbbc38be58
Julian Ventura <zs22017046@estudiantes.uv.mx>
2024-03-14 10:40:16 -0600

    ajustes actividad

--- ejemplo2/src/main/java/uv/herenciapoo/Main.java
+++ ejemplo2/src/main/java/uv/herenciapoo/Main.java
@@ -1,36 +1,23 @@
 package main.java.uv.herenciapoo;
 
 public class Main {
-    public static void main(String[] args) {
-        // Rectangle class
-        Rectangle rectangle = new Rectangle();
-        double width = 3;
-        double height = 5;
-
-        rectangle.setWidth(width);
-        rectangle.setHeight(height);
-        
+    
+    public static void printRectangle(Rectangle rectangle) {
         System.out.println("Rectangle");
-        System.out.println("Number of sides = " + rectangle.getNumberOfSides());
-        System.out.println("width = " + rectangle.getWidth() + "\theight = " + rectangle.getHeight());
-        System.out.println("Area = " + rectangle.getArea() + "\tPerimeter = " + rectangle.getPerimeter());
-        System.out.println("---------------------");
+        System.out.printf("width = %.2f\theight = %.2f\n", rectangle.getWidth(), rectangle.getHeight());
+        System.out.printf("area = %.2f\tperimeter = %.2f\n", rectangle.getArea(), rectangle.getPerimeter());
+    }
 
-        int factor = 2;
-        rectangle.redimension(factor);
-        System.out.println("After redimension by " + factor);
-        System.out.println("---------------------");
+    public static void main(String[] args) {
+        Rectangle rectangle = new Rectangle();
+        rectangle.setWidth(4);
+        rectangle.setHeight(3);
 
-        System.out.println("width = " + rectangle.getWidth() + "\theight = " + rectangle.getHeight());
-        System.out.println("Area = " + rectangle.getArea() + "\tPerimeter = " + rectangle.getPerimeter());
+        printRectangle(rectangle);
 
-        
-        // Triangle class
-        Triangle triangle = new Triangle();
-        triangle.setWidth(3);
-        triangle.setHeight(4);
+        rectangle.redimension(3);
+        System.out.println("---- After redimension ----");
 
-        System.out.println("\nTriangle\nNumber of sides = " + triangle.getNumberOfSides());
-        System.out.println("Area = " + triangle.getArea() + "\tPerimeter = " + triangle.getPerimeter());
+        printRectangle(rectangle);
     }
 }
